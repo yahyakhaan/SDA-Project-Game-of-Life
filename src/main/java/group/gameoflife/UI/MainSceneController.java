c4ec409967308d3d81a7e87dad05ad32640bdda4
AbdulAli-FastNU <l190917@lhr.nu.edu.pk>
2021-11-17 19:31:55 +0500

    Linked UI with main BL, Implemented some UI Functionality (Grid of cells, OnClickActions))

--- src/main/java/group/gameoflife/UI/MainSceneController.java
+++ src/main/java/group/gameoflife/UI/MainSceneController.java
@@ -1,10 +1,25 @@
 package group.gameoflife.UI;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.ZoomEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
 
 public class MainSceneController {
+    private int check;
+    private  GridPane Grid_;
     private Graphical_UI GUI;
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private AnchorPane plane;
     @FXML
     private Label welcomeText;
 
@@ -15,6 +30,91 @@ public class MainSceneController {
 
     public void loadGUI(Graphical_UI GUI)
     {
+        check =1;
         this.GUI=GUI;
+        int[] gridSize= GUI.getGridSize();
+        System.out.println("GridSize from GRID: "+ gridSize[0]);
+    }
+    @FXML
+    public void updateCells()
+    {
+        int gridSize[];
+        gridSize = GUI.getGridSize();
+        for (int r = 0; r < gridSize[0]; r++) {
+            for (int c = 0; c < gridSize[1]; c++) {
+                Grid_.getChildren();
+
+            }
+        }
+
+    }
+    public void clickOnButton(ActionEvent e)
+    {
+        int buttonID;
+        Button b = (Button)e.getSource();
+        System.out.println(b.getId()+" IS CLICKED");
+
+    }
+    public void loadBlankGrid()
+    {
+        System.out.println(check);
+        int[] gridSize;
+        gridSize = this.GUI.getGridSize();
+        Grid_ = new GridPane();
+        Grid_.setPadding(new Insets(5));
+        Grid_.setHgap(5);
+        Grid_.setVgap(5);
+
+        for (int r = 0; r < gridSize[0]; r++) {
+            for (int c = 0; c < gridSize[1]; c++) {
+                System.out.println("In loop");
+                Button button = new Button(String.valueOf("  "));
+                button.setId(Integer.toString(r)+":"+Integer.toString(c));
+                button.setStyle("-fx-background-color: #FFF;");
+                System.out.println(button.getId());
+                button.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        int buttonID;
+                        if (button.getStyle()=="-fx-background-color: #FFF;") {
+                            //Button Selected
+                            Button b = (Button) actionEvent.getSource();
+                            System.out.println(b.getId() + " IS CLICKED");
+                            b.setStyle("-fx-background-color: #00FF00;");
+                        }
+                        else if (button.getStyle()=="-fx-background-color: #00FF00;") {
+                           //Button Deselected
+                            Button b = (Button) actionEvent.getSource();
+                            System.out.println(b.getId() + " IS CLICKED");
+                            b.setStyle("-fx-background-color: #FFF;");
+                        }
+                    }
+                });
+                Grid_.add(button, c, r);
+            }
+        }
+        //this.scrollpane.setScaleY(10);
+      //  plane.setVisible(false);
+        this.scrollPane.setContent(Grid_);
+        //this.scrollPane.();
+
+
+
+    }
+    public void start(ActionEvent e)
+    {
+        welcomeText.setText("Game Started");
+        loadBlankGrid();
+
+    }
+    public void doit(ActionEvent e)
+    {
+        welcomeText.setText("GG");
+
+    }
+    public void nextStage(ActionEvent e)
+    {
+
+
     }
 }
--- src/main/java/group/gameoflife/main.java
+++ src/main/java/group/gameoflife/main.java
@@ -4,8 +4,13 @@ import group.gameoflife.BL.grid;
 import group.gameoflife.UI.Graphical_UI;
 import group.gameoflife.UI.MainSceneController;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,12 +18,21 @@ import java.io.IOException;
 public class main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        int grid_xSize=10;
-        int grid_ySize=10;
+        int grid_xSize=5;
+        int grid_ySize=5;
+
+
+
+
         grid game = new grid(grid_xSize,grid_ySize);
         Graphical_UI GUI = new Graphical_UI(game);
         FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("MainScene.fxml"));
+
         MainSceneController Controller = new MainSceneController();
+        fxmlLoader.setController(Controller);
+        //Controller.loadBlankGrid();
+        //ActionEvent e = new ActionEvent();
+        //Controller.doit(e);
         Controller.loadGUI(GUI);
         Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         stage.setTitle("Game Of Life");
